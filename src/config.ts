e316342b0ff9dee69219f85e1a5b128c214e7b12
iVanology <peng.ivan@proton.me>
2024-03-19 23:39:45 +0800

    update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Hi there. I'm Ivan";
-export const SITE_DESCRIPTION = "HI there";
+export const SITE_TITLE = "Ivan";
+export const SITE_DESCRIPTION = "Hi there";
 // const GENERATE_SLUG_FROM_TITLE set to false allow to use Chinese title
 export const GENERATE_SLUG_FROM_TITLE = false;
 export const TRANSITION_API = true;
